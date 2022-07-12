fe77b109ea27cc6323d890c9c0ccc90040772051
Ju-yeop <dhwnduq12@naver.com>
2022-07-12 23:09:15 +0900

    User CRUD

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # Nuber Eats
 
-### The Backend of Nuber Eats Clone
+## The Backend of Nuber Eats Clone
+
+## User Model:
+
+- id
+- createdAt
+- updatedAt
+
+- email
+- password
+- role(client|owner|delivery)
+
+## User CRUD:
+
+- Create Account
+- Log In
+- See Profile
+- Edit Profile
+- Verify Email
 
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
---
+++ src/users/users.resolver.ts
@@ -0,0 +1,46 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
+import { LoginInput, LoginOutput } from './dtos/login.dto';
+import { User } from './entities/user.entity';
+import { UserService } from './users.service';
+
+@Resolver((of) => User)
+export class UserResolver {
+  constructor(private readonly usersService: UserService) {}
+
+  @Query((returns) => Boolean)
+  hi() {
+    return true;
+  }
+
+  @Mutation((returns) => CreateAccountOutput)
+  async createAccount(
+    @Args('input') CreateAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      return this.usersService.createAccount(CreateAccountInput);
+    } catch (error) {
+      return {
+        error,
+        ok: false,
+      };
+    }
+  }
+
+  @Mutation((type) => LoginOutput)
+  async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+    try {
+      return this.usersService.login(loginInput);
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
+}
