fc3555c64f77b1cb4169594a72144dfac0071095
Ju-yeop <dhwnduq12@naver.com>
2022-07-22 21:05:54 +0900

    Before Custom Repository

---
+++ src/restaurants/repositories/category.repository.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+import { Repository } from 'typeorm';
+import { Category } from '../entities/category.entity';
+
+export class CategoryRepository extends Repository<Category> {}
